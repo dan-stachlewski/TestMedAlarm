7b779450f1f455cf82b5a0924da469e9faa101f4
dan-stachlewski <dan.stachlewski@gmail.com>
2018-05-08 23:17:26 +1000

    I created a SampleDataProvider that uses either List<> or Map<> Views to retrieve and display data in an Activity. We used the List Class to do this displaying the name of an item on the Activity retrieved by its Id. The next step is to use a better method like RecyclerView to display data to an activity.

--- app/src/main/java/com/example/dnafv/testmedalarm/MainActivity.java
+++ app/src/main/java/com/example/dnafv/testmedalarm/MainActivity.java
@@ -11,13 +11,23 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.dnafv.testmedalarm.model.DataItem;
+import com.example.dnafv.testmedalarm.sampleData.SampleDataProvider;
 
 import org.w3c.dom.Text;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     //Initialise the variable used to change the Text within the TextView in the MainActivity
     // Activity activity_main.xml > content_main.xml
     TextView tvOut;
+    //Create a reference to the list version of the data as a persistent field
+    //Get a refrence to that list by calling the static field of the sampleDataProvider
+
+    //Now we have all that data avaliable to the Activity Class
+    List<DataItem>dataItemList = SampleDataProvider.dataItemList;
 
 
 
@@ -45,13 +55,12 @@ public class MainActivity extends AppCompatActivity {
 
         //We can now Test the dataItem Class:
         /**
-         * Declare a new instance of the dataItem class
+         * Declare a new single instance of the dataItem class
          * the Object will be named item
          * use a version of the Constructor method DataItem() that receives all of the values
          * DataItems have been successfully saved and displayed in the app via the DataItem Class
          * */
-        DataItem item =
-                new DataItem(null, "My menu item", "a category", "a description", 1, 9.95, "apple_pie.jpg");
+        //DataItem item = new DataItem(null, "My menu item", "a category", "a description", 1, 9.95, "apple_pie.jpg");
 
 
         //This links the variable tvOut to the TextView within the MainActivity
@@ -60,7 +69,23 @@ public class MainActivity extends AppCompatActivity {
         //This changes the text being displayed from Hello World! to Ready to code in the TextView
         // within the MainActivity Activity activity_main.xml > content_main.xml
         //tvOut.setText("Ready to code!");
-        tvOut.setText(item.toString());
+        tvOut.setText("");
+
+        //To sort the data alphabetically
+        //Sorting Complext Objects - need an insance of Java's comparator interface
+        Collections.sort(dataItemList, new Comparator<DataItem>() {
+            @Override
+            public int compare(DataItem o1, DataItem o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+
+        //Loop thru the data with a foreach loop
+        //Using an instance of the DataItem class called item that I will get from the dataItemList
+        for (DataItem item: dataItemList) {
+            tvOut.append(item.getName() + "\n");
+        }
     }
 
     //This method loads the menu that sits at the top of the device
