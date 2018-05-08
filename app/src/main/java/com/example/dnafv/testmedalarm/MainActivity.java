25fe577f311363680bd3bdbcc3f1df6802e35f74
dan-stachlewski <dan.stachlewski@gmail.com>
2018-05-09 09:25:51 +1000

    In this part we created a way for the app to display our data that is saved in a file as simple strings in a List View. Thus we are extracting the strings from a list of complex data objects to create a list of simple strings. We are using the ArrayAdapter to pass that data (strings) directly to the ListView. The result is a scrollable list of Item names on the MainActivity - sorted in alphabetical order.

--- app/src/main/java/com/example/dnafv/testmedalarm/MainActivity.java
+++ app/src/main/java/com/example/dnafv/testmedalarm/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.dnafv.testmedalarm.model.DataItem;
@@ -15,6 +17,7 @@ import com.example.dnafv.testmedalarm.sampleData.SampleDataProvider;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -22,14 +25,19 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     //Initialise the variable used to change the Text within the TextView in the MainActivity
     // Activity activity_main.xml > content_main.xml
-    TextView tvOut;
+    //TextView tvOut;
     //Create a reference to the list version of the data as a persistent field
     //Get a refrence to that list by calling the static field of the sampleDataProvider
 
+    //We are working with a single Object made
     //Now we have all that data avaliable to the Activity Class
+    // as we are working with simple string sets made up of item data
     List<DataItem>dataItemList = SampleDataProvider.dataItemList;
 
 
+    //New List made up of simple string values & use the foreach loop to cycle through each datum
+    //in the array
+    List<String>itemNames = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,27 +73,45 @@ public class MainActivity extends AppCompatActivity {
 
         //This links the variable tvOut to the TextView within the MainActivity
         // Activity activity_main.xml > content_main.xml
-        tvOut = (TextView)findViewById(R.id.tvOut);
+        //tvOut = (TextView)findViewById(R.id.tvOut);
         //This changes the text being displayed from Hello World! to Ready to code in the TextView
         // within the MainActivity Activity activity_main.xml > content_main.xml
         //tvOut.setText("Ready to code!");
-        tvOut.setText("");
+        //tvOut.setText("");
 
         //To sort the data alphabetically
         //Sorting Complext Objects - need an insance of Java's comparator interface
-        Collections.sort(dataItemList, new Comparator<DataItem>() {
+
+        /*Collections.sort(dataItemList, new Comparator<DataItem>() {
             @Override
             public int compare(DataItem o1, DataItem o2) {
                 return o1.getName().compareTo(o2.getName());
             }
         });
-
+        */
 
         //Loop thru the data with a foreach loop
         //Using an instance of the DataItem class called item that I will get from the dataItemList
         for (DataItem item: dataItemList) {
-            tvOut.append(item.getName() + "\n");
+            //tvOut.append(item.getName() + "\n");
+            itemNames.add(item.getName());
         }
+        //B/c we are using simple Strings I can sort thru them using Collections
+        // whih are now ready for display
+        Collections.sort(itemNames);
+
+        //In order to display data in a list view we need to use a class called an
+        // Adapter = ArrayAdapter whose job is to cycle thru the adapter and then as needed
+        // create View Objects to represent List Items - rows in the list & then pass the data
+        // to those List Items so they are displayed in the Activity
+        //At the mo the ArrayAdapter is managing String values, named adapter & will instantiate
+        // it with a call to the ArrayAdapters Constructor Method:
+        // Context = this (for current activity), layout resorce id (layout included by android), pass in dataObjects
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemNames);
+        //Now we have to bind the adapter to the list view with a ref to the listView Object
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        //Bind the 2 objects together
+        listView.setAdapter(adapter);
     }
 
     //This method loads the menu that sits at the top of the device
